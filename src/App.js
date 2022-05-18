848a23e35a910b250215d3772018d1cb795cd52b
dario <darioh.dev@gmail.com>
2022-05-18 17:56:58 -0300

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div classNa me="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          asdasd Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
